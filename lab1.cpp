a2399cc3a64cd4e9cf498aef32a9415d67c2e1a1
Иван Непряхин <watamote@Ivans-MacBook-Air.local>
2025-10-05 17:14:42 +0300

    Initial initial commit

---
+++ lab1.cpp
@@ -0,0 +1,195 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    string data;
+    Node* prev;
+    Node* next;
+    Node(string& val) : data(val), prev(nullptr), next(nullptr) {};
+};
+
+class LinkedList {
+private:
+    Node* head;
+    Node* tail;
+public:
+    LinkedList() : head(nullptr), tail(nullptr) {};
+    void push(string value) {
+        Node* current = new Node(value);
+        if (!head) {
+            head = tail = current;
+        } 
+        else {
+            tail -> next = current;
+            current -> prev = tail;
+            tail = current;
+        }
+    }
+    void rm(string value) {
+        Node * current = head;
+        while (current) {
+            if (current -> data == value) {
+                if (current -> prev) current -> prev -> next = current -> next;
+                else head = current -> next;
+                if (current -> next) current -> next -> prev = current -> prev;
+                else tail = current -> prev;
+                delete current;
+                return;
+            }
+            current = current -> next;
+        }
+    }
+    void printList() {
+        Node *current = head;
+        while (current) {
+            cout << current -> data << endl;
+            current = current -> next;
+        }
+    }
+    string pop() {
+        if (!tail) throw runtime_error("Empty");
+        string output = tail -> data;
+
+        Node* temp = tail;
+        tail = tail->prev;
+
+        if (tail)
+            tail->next = nullptr;
+        else
+            head = nullptr;
+
+        delete temp;
+        return output;
+    }
+    string showTail() {
+        if (!tail) throw runtime_error("Empty");
+        return tail->data;
+    }
+
+    bool isEmpty() {
+        return head == nullptr;
+    }
+
+    ~LinkedList() {
+        Node *current = head;
+        while (current) {
+            Node* nextNode = current -> next;
+            delete current;
+            current = nextNode;
+        }
+        head = tail = nullptr;
+    }
+
+};
+
+class Stack {
+private:
+    LinkedList list;
+public:
+    void pushStack(string val) {list.push(val);}
+    void pop() {list.pop();}
+    string top() {return list.showTail();}
+    bool isEmpty() {return list.isEmpty();}
+};
+
+int priority(string op) {
+    if (op == "+" || op == "-") return 1;
+    if (op == "/" || op == "*") return 2;
+    if (op == "^") return 3;
+    if (op == "sin" || op == "cos") return 4;
+}
+
+bool isOperator(string token) {
+    return token == "+" || token == "-" || token == "*" || token == "/" 
+    || token == "^";
+}
+
+bool isFunction(string token) {
+    return token == "sin" || token == "cos";
+}
+
+bool isRightAs(string op) {
+    return op == "^";
+}
+
+void toRPN(const char* expression, string output[100], int& outPos) {
+    Stack stack;
+    outPos = 0;
+    int i = 0;
+    while (expression[i] != '\0') {
+        if (isspace(expression[i])) {
+            i++;
+            continue;
+        }
+        if (isdigit(expression[i])) {
+            string number; // since number is a sequence of digits ig
+            while (isdigit(expression[i])) {
+                number += expression[i++];
+
+            }
+            output[outPos++] = number;
+        }
+        else if (isalpha(expression[i])) {
+            string trigFunc;
+            while (isalpha(expression[i])) {
+                trigFunc += expression[i++];
+                if (trigFunc == "cos" || trigFunc == "sin") {
+                    stack.pushStack(trigFunc);
+                }
+                else {throw runtime_error("Ne sinus i ne cosinus bratan");}
+            }
+        }
+        else if (expression[i] == '(') {
+            stack.pushStack("(");
+            i++;
+        }
+        else if (expression[i] == ')') {
+            while (stack.top() != "(" && !stack.isEmpty()) {
+                output[outPos++] = stack.top();
+                stack.pop();
+            }
+            if (stack.top() == "(" && !stack.isEmpty()) {
+                stack.pop();
+            }
+            if (isFunction(stack.top()) && !stack.isEmpty()) {
+                output[outPos++] = stack.top();
+                stack.pop();
+            }
+            i++;
+        }
+        else {
+            string oper(1, expression[i]);
+            while (!stack.isEmpty() && (isOperator(stack.top()) || isFunction(stack.top()) && (((!isRightAs(oper) && priority(oper) <= priority(stack.top())) ||
+            (isRightAs(oper) && priority(oper) <  priority(stack.top())))))) {
+                output[outPos++] = stack.top();
+                stack.pop();
+            }
+            stack.pushStack(oper);
+            i++;
+        }
+
+
+    }
+    while (!stack.isEmpty()) {
+        output[outPos++] = stack.top();
+        stack.pop();
+    }
+
+}
+
+
+
+int main() {
+    LinkedList list;
+    const char *expression = "3 + 4 * 2 / (1 - 5) ^ 2 ^ 3";
+    string output[100];
+    int count = 0;
+    toRPN(expression, output, count);
+    cout << "RPN" << endl;
+    for (int i = 0; i < count; ++i) {
+        cout << output[i] << " ";
+    }
+
+    return 0;
+}
